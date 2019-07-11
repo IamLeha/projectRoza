dac6eb7949ab7ba12819e223444d4aa6dd54673d
Dima <dimonchik_dyatchin6464@mail.ru>
2019-07-11 14:39:06 +0300

    home for desctop

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let cross = document.getElementById('close');
 cross.onclick = function() {
     cross.parentElement.remove();
 };
+
+$('.main-slider').slick({
+    arrows: true,
+    prevArrow: $(".prev"),
+    nextArrow: $(".next"),
+    infinite: true,
+	dots: true
+});
